234a8b66ff4bdb21a92e018dcfa1b97d8da791e2
HGibz <HGibz@users.noreply.github.com>
2021-03-24 15:53:29 +0000

    mask, counter

---
+++ MaskClicker/Assets/Scripts/GlobalMasks.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class GlobalMasks : MonoBehaviour
+{
+    public static int MaskCount;
+    public GameObject MaskDisplay;
+    public int InternalMask;
+
+    void Update()
+    {
+        InternalMask = MaskCount;
+        MaskDisplay.GetComponent<Text>().text = "Masks: " + InternalMask;
+    }
+}
---
+++ MaskClicker/Assets/Scripts/NewBehaviourScript.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    public GameObject textBox;
+
+    public void ClickTheButton ()
+    {
+        GlobalMasks.MaskCount += 1;
+    }
+}
