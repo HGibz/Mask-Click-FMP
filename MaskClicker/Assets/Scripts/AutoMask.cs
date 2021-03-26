29030687d144d2dfda7d4872e634a40ce490bf91
HGibz <HGibz@users.noreply.github.com>
2021-03-26 15:50:58 +0000

    auto clicker

---
+++ MaskClicker/Assets/Scripts/AutoMask.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AutoMask : MonoBehaviour
+{
+
+    public bool CreatingMask = false;
+    public static int MaskIncrease = 1;
+    public int InternalIncrease;
+    
+
+    
+    void Update()
+    {
+        InternalIncrease = MaskIncrease;
+        if (CreatingMask == false)
+        {
+            CreatingMask = true;
+            StartCoroutine(CreateTheMask());
+        }
+    }
+
+    IEnumerator CreateTheMask()
+    {
+        GlobalMasks.MaskCount += InternalIncrease;
+        yield return new WaitForSeconds(1);
+        CreatingMask = false;
+    }
+}
